8ada57cddd50500a49f95101b0311eb5b7aa6810
Fernando Pachon <josep3194@gmail.com>
2022-11-28 11:36:43 -0500

    feat:quick order

---
+++ react/components/QuickOrder.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { useMutation, useLazyQuery } from "react-apollo";
+import UPDATE_CART from "../graphql/updateCart.graphql"
+import GET_PRODUCT from "../graphql/getProductBySku.graphql"
+import { useCssHandles } from 'vtex.css-handles'
+import "./styles.css"
+
+const QuickOrder = () => {
+  const [inputText, setInputText] = useState("");
+  const [search, setSearch] = useState("");
+  const [getProductData, { data: product }] = useLazyQuery(GET_PRODUCT)
+  const [addToCart] = useMutation(UPDATE_CART)
+
+  const CSS_HANDLES = [
+    "quickOrder_container",
+    "quickOrder_form",
+    "quickOrder_label-container",
+    "quickOrder_label",
+    "quickOrder_input",
+    "quickOrder_input-btn"
+  ]
+  const handles = useCssHandles(CSS_HANDLES)
+
+  const handleChange = (evt: any) => {
+    setInputText(evt.target.value)
+    console.log("input changed", inputText);
+  }
+  useEffect(() => {
+    console.log("El resultado de mi producto es:", product, search)
+    if (product) {
+      let skuId = parseInt(inputText)
+      addToCart({
+        variables: {
+          salesChannel: "1",
+          items: [
+            {
+              id: skuId,
+              quantity: 1,
+              seller: "1"
+            }
+          ]
+        }
+      })
+        .then(() => {
+          window.location.href = "/checkout"
+        })
+    }
+  }, [product, search])
+  const addProductToCart = () => {
+    //ingresar declaración de la mutación
+    getProductData({
+      variables: {
+        sku: inputText
+      }
+    })
+  }
+  const searchProduct = (evt: any) => {
+    evt.preventDefault();
+    if (!inputText) {
+      alert("gato")
+    } else {
+      //Busqueda, data del producto
+      setSearch(inputText)
+      addProductToCart()
+    }
+  }
+  return <div className={handles["quickOrder_container"]}>
+    <h2> Easy fast</h2>
+    <form className={handles["quickOrder_form"]} onSubmit={searchProduct}>
+      <div className={handles["quickOrder_label-container"]} >
+        <label className={handles["quickOrder_label"]} htmlFor="sku">Ingresa un SKU:</label>
+        <input className={handles["quickOrder_input"]} id="sku" type="text" onChange={handleChange}></input>
+      </div>
+      <input className={handles["quickOrder_input-btn"]} type="submit" value="Añadir al carrito" />
+    </form>
+  </div>
+}
+
+export default QuickOrder
